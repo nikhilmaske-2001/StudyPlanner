c891c904aed4939f4bb495e93a542530e5025944
nikhilmaske-2001 <59444243+nikhilmaske-2001@users.noreply.github.com>
2022-09-06 11:10:08 +0530

    Login Page completed.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
+import Login from './pages/Login/Login';
 import Register from './pages/Register/Register';
 
 function App() {
   return (
     <div className="App">
-      <Register />
+      {/* <Register /> */}
+      <Login />
     </div>
   );
 }
---
+++ src/pages/Login/Login.jsx
@@ -0,0 +1,55 @@
+import React from "react";
+import "./Login.css";
+import avatar from "../../images/avatar.png";
+
+function Login() {
+  return (
+    <div>
+      <h2>Login Form</h2>
+      <form>
+        <div className="imgcontainer">
+          <img src={avatar} alt="Avatar" className="avatar" />
+        </div>
+
+        <div className="container">
+          <label for="uname">
+            <b>Username</b>
+          </label>
+          <input
+            type="text"
+            placeholder="Enter Username"
+            name="uname"
+            required
+          />
+
+          <label for="psw">
+            <b>Password</b>
+          </label>
+          <input
+            type="password"
+            placeholder="Enter Password"
+            name="psw"
+            required
+          />
+
+          <button type="submit">Login</button>
+          <label>
+            <input type="checkbox" checked="checked" name="remember" /> Remember
+            me
+          </label>
+        </div>
+
+        <div className="container">
+          <button type="button" className="cancelbtn">
+            Cancel
+          </button>
+          <span className="psw">
+            Forgot <a href="#">password?</a>
+          </span>
+        </div>
+      </form>
+    </div>
+  );
+}
+
+export default Login;
