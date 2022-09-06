6532b9f89b61572f770bcd222cbce4faaa262983
nikhilmaske-2001 <59444243+nikhilmaske-2001@users.noreply.github.com>
2022-09-06 11:39:23 +0530

    Sessionlist page done

--- src/dummyData/sessionslist.js
+++ src/dummyData/sessionslist.js
@@ -22,103 +22,23 @@ export const sessions = [
     },
     {
         name: "English crew",
-        id: 1,
+        id: 4,
         date: "23/06/2023",
         starttime: "10AM",
         endtime: "12PM"
     },
     {
         name: "Maths gang",
-        id: 2,
+        id: 5,
         date: "24/06/2023",
         starttime: "11AM",
         endtime: "12PM"
     },
     {
         name: "Science nerds",
-        id: 3,
+        id: 6,
         date: "25/06/2023",
         starttime: "1PM",
         endtime: "4PM"
-    }, {
-        name: "English crew",
-        id: 1,
-        date: "23/06/2023",
-        starttime: "10AM",
-        endtime: "12PM"
-    },
-    {
-        name: "Maths gang",
-        id: 2,
-        date: "24/06/2023",
-        starttime: "11AM",
-        endtime: "12PM"
-    },
-    {
-        name: "Science nerds",
-        id: 3,
-        date: "25/06/2023",
-        starttime: "1PM",
-        endtime: "4PM"
-    }, {
-        name: "English crew",
-        id: 1,
-        date: "23/06/2023",
-        starttime: "10AM",
-        endtime: "12PM"
-    },
-    {
-        name: "Maths gang",
-        id: 2,
-        date: "24/06/2023",
-        starttime: "11AM",
-        endtime: "12PM"
-    },
-    {
-        name: "Science nerds",
-        id: 3,
-        date: "25/06/2023",
-        starttime: "1PM",
-        endtime: "4PM"
-    }, {
-        name: "English crew",
-        id: 1,
-        date: "23/06/2023",
-        starttime: "10AM",
-        endtime: "12PM"
-    },
-    {
-        name: "Maths gang",
-        id: 2,
-        date: "24/06/2023",
-        starttime: "11AM",
-        endtime: "12PM"
-    },
-    {
-        name: "Science nerds",
-        id: 3,
-        date: "25/06/2023",
-        starttime: "1PM",
-        endtime: "4PM"
-    }, {
-        name: "English crew",
-        id: 1,
-        date: "23/06/2023",
-        starttime: "10AM",
-        endtime: "12PM"
-    },
-    {
-        name: "Maths gang",
-        id: 2,
-        date: "24/06/2023",
-        starttime: "11AM",
-        endtime: "12PM"
-    },
-    {
-        name: "Science nerds",
-        id: 3,
-        date: "25/06/2023",
-        starttime: "1PM",
-        endtime: "4PM"
-    },
+    }
 ];
--- src/pages/components/Session/Session.jsx
+++ src/pages/components/Session/Session.jsx
@@ -1,11 +1,33 @@
 import React from "react";
+import Box from "@mui/material/Box";
+import Card from "@mui/material/Card";
+import CardActions from "@mui/material/CardActions";
+import CardContent from "@mui/material/CardContent";
+import Button from "@mui/material/Button";
+import Typography from "@mui/material/Typography";
 
 function Session(session) {
   return (
     <div>
-      <h1>
-        <li>{session.data.name}</li>
-      </h1>
+      <Card sx={{ minWidth: 275 }}>
+        <CardContent>
+          <Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>
+            {session.data.id}
+          </Typography>
+          <Typography variant="h5" component="div">
+            {session.data.name}
+          </Typography>
+          <Typography sx={{ mb: 1.5 }} color="text.secondary">
+            Date: {session.data.date}
+          </Typography>
+          <Typography variant="body2">
+            Timing: {session.data.starttime} to {session.data.endtime}
+          </Typography>
+        </CardContent>
+        <CardActions>
+          <Button size="small">Join session</Button>
+        </CardActions>
+      </Card>
     </div>
   );
 }
