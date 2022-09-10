ff9bdf13cb9f73fcd2e499cd5e62667f43c8585e
nikhilmaske-2001 <59444243+nikhilmaske-2001@users.noreply.github.com>
2022-09-10 16:56:20 +0530

    Router working.

--- client/src/App.js
+++ client/src/App.js
@@ -3,15 +3,22 @@ import Login from './pages/Login/Login';
 import NewSession from './pages/NewSession/NewSession';
 import Register from './pages/Register/Register';
 import SessionList from './pages/SessionsList/SessionList';
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+} from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <Register />
-      <Login />
-      <SessionList />
-      <NewSession />
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/sessions" element={<SessionList />} />
+        <Route path="/newSession" element={<NewSession />} />
+      </Routes>
+    </Router>
   );
 }
 
