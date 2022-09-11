6f75d0235dd0c3b889b55799142cc1f9e3d572f3
nikhilmaske-2001 <59444243+nikhilmaske-2001@users.noreply.github.com>
2022-09-11 08:23:47 +0530

    Show user email at top if user is login in.

--- client/src/pages/SessionsList/SessionList.jsx
+++ client/src/pages/SessionsList/SessionList.jsx
@@ -1,14 +1,17 @@
-import React from "react";
+import React, { useContext } from "react";
 import Button from "@mui/material/Button";
 import Session from "../components/Session/Session";
 import useFetch from "../../hooks/useFetch";
 import { Link } from "react-router-dom";
+import { AuthContext } from "../../context/AuthContext";
 
 function SessionList() {
   const { data, loading, error } = useFetch("/sessions");
+  const { user } = useContext(AuthContext);
 
   return (
     <div className="container">
+      {user ? user.email : "User not login"}
       <h1>Study Sessions</h1>
       <Link to="/newSession">
         <Button size="large" className="new-session">
